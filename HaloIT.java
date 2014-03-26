045ece9a7ffb14d495574d70f49f921d50795d28
unknown <semangatmalik@gmail.com>
2014-03-26 18:59:58 +0700

    commit02

---
+++ HaloIT.java
@@ -0,0 +1,10 @@
+//Perintah penggunaan package versi 01
+package Prak02.bin;
+
+public class HaloIT
+{
+	public static void main (String[] Malik)
+	{
+		System.out.println("Jurusan Teknik Informatika");
+	}
+}
